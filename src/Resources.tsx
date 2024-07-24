38b1f81780ae2f31a1477cf95ba7c6398e4ffd77
pricet2023 <pricet1280@gmail.com>
2024-07-24 12:33:16 +0100

    added images for resource page, changed fonts and spaced out buttons

--- src/Resources.tsx
+++ src/Resources.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import Banner from './Banner.tsx';
 import './Resources.css'
+import phone from './images/phone.PNG';
+import resources from './images/resources.PNG';
+import speak from './images/speak.PNG';
 
 const Resources = () => (
   <div>
     <Banner />
     <div className='Resources'>
       <h1 className='title'>Resources</h1>
-      <div className='ButtonContainer'>
-        <button className='ResourceButton'>Chat Room</button>
-        <button className='ResourceButton'>Resources and Support</button>
-        <button className='ResourceButton'>Book a talk</button>
+      <div className='ActionsContainer'>
+        <div className='Action'>
+          <img src={phone} alt='phone'/>
+          <button className='ResourceButton'>Chat Room</button>
+        </div>
+        <div className='Action'>
+          <img src={resources} alt='resources'/>
+          <button className='ResourceButton'>Resources and Support</button>
+        </div>
+        <div className='Action'>
+          <img src={speak} alt='speak'/>
+          <button className='ResourceButton'>Book a talk</button>
+        </div>
       </div>
     </div>
   </div>
